dfcd18fffbcc668db06925f82ec67b8b211cccd0
Sujeeth-P <sujeeth9487@gmail.com>
2025-06-28 20:52:19 +0530

    Retrive

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,10 @@
 import express from 'express'
 
-import { registerUser, loginUser, createOrder, getUserOrders, updateOrderStatus } from '../controller/authController.js'
+import { registerUser, loginUser } from '../controller/authController.js'
 
 
 const route = express.Router()
 route.post("/signup", registerUser)
 route.post("/login", loginUser)
-
-// Order routes
-route.post("/orders", createOrder)
-route.get("/orders/:userId", getUserOrders)
-route.put("/orders/:userId/:orderId", updateOrderStatus)
 
 export default route;
